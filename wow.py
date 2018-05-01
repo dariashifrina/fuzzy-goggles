3cf9cb9a98b1e8087117c84dee50d2a6559e83b6
Charles <cweng@stuy.edu>
2018-04-30 22:27:28 -0400

    stuffs for you

--- wow.py
+++ wow.py
@@ -1,37 +1,52 @@
+# Charles Weng Dasha Shifrina
+# SoftDev2   pd7
+# K #18: Reductio ad Absurdum
+# 2018-4-25
+
+################################################################################
+#                                Initializtion                                 #
+################################################################################
+
 from functools import reduce
-
-#print reduce((lambda x, y: x + 1), [1,2,3])
-
 
 f = open("book.txt", "r")
 book = f.read()
 words = book.split()
 
-#print words
-z = 0
 
+################################################################################
+#                                   Functions                                  #
+################################################################################
 
 def word_freq(book, word):
-    freq = [1 for x in book if x.lower() == word]
-    if len(freq) == 0:
-        freq = [0,0]
-    return reduce((lambda x,y: x + y), freq)
+    freq = [1 if x.lower() == word else 0 for x in book]
+    return reduce((lambda x, y: x + y), freq)
 
 
 def group_freq(book, word_list):
-    freq = [word_freq(book,x) for x in word_list]
-    return reduce((lambda x,y: x + y), freq)
+    freq = [word_freq(book, x) for x in word_list]
+    return reduce((lambda x, y: x + y), freq)
 
+
+# direct bash approach; not sure how to optimize
 def most_freq(book):
-    freq = [(x,word_freq(book, x)) for x in book]
+    freq = [(x, word_freq(book, x)) for x in book]
     return max(freq)[0]
-    
 
+
+################################################################################
+#                                   Testing                                    #
+################################################################################
+
+print "hi"
 print("frequency of 'the' in the book:")
 print word_freq(words, "the")
 print("frequency of 'dog' in the book:")
 print word_freq(words, "dog")
 print("frequency of 'the' and 'dog' in the book:")
 print group_freq(words, ["the", "dog"])
-print("most frequent word:")
-print most_freq(words)
+
+# most_freq takes a very long time to process
+
+# print("most frequent word:")
+# print most_freq(words)
